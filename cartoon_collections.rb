892e698585ae49d988c7307d9aec4f7d291dad1e
ariel murphy <arielladders@gmail.com>
2020-07-17 15:20:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,8 +13,8 @@ def long_planeteer_calls(array)
 end
 
 cheese_types = ["cheddar", "gouda", "camembert"]
-def find_the_cheese(array)
-  array.find do |cheese|
+def find_the_cheese(cheese_types)
+  cheese_types.find do |cheese|
     cheese_types.include? (cheese)
   end
 end
