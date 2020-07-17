b1f959595d0980c026a336b27719c219828c626d
ariel murphy <arielladders@gmail.com>
2020-07-17 15:18:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def long_planeteer_calls(array)
   array.any? {|call| call.length > 4}
 end
 
-def find_the_cheese(cheese_types)
-  
+def find_the_cheese(array)
+  array.find do |cheese|
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
